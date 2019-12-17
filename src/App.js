a6c404610c8617574f0303c5ff1cd445665e10a6
skooger <adamjskooglambda@gmail.com>
2019-12-17 11:24:05 -0800

    pushing from local, completed MVP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from "react-router-dom";
 import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navigation from "./components/Navigation";
 
 const App = () => (
+  
   <div>
     <Navigation />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About/>
+      </Route>
+      <Route path="/contact">
+        <Contact/>
+      </Route>
+    </Switch>
+  
   </div>
 );
 
